14f58f76cfee68ed864513f50693de52da26c6b5
Majid Burney <moocow@euphoricsoup.com>
2015-05-13 00:28:15 -0700

    Add editorconfig

--- type-hints.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview A library of jsdoc type definitions to inform IDEA about implicit or global types that it can't
- * infer from sources.
- */
-
-/**
- * @description Node.js readable stream
- * @typedef {function} stream.Readable
- */
